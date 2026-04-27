01409aef44dadff96da24539db58b87f4a75d74d
md-shakib-hassan <arefinshakibkhan@gmail.com>
2026-04-28 01:07:32 +0600

    added second project allmost done

--- CounteriesProject/src/Counteries/Counteries.jsx
+++ CounteriesProject/src/Counteries/Counteries.jsx
@@ -1,19 +1,36 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import CountryName from './CountryName';
+import './GridCounteries.css'
 
 const Counteries = ({countryPromise}) => {
 
     const data = use(countryPromise);
+    const [visitedCounteries,setVisitedcounteries] = useState([]);
+
+    const handleVisitedCountries=(name)=>{
+        const newCountries = [...visitedCounteries,name];
+        setVisitedcounteries(newCountries);
+    }
+
 
     return (
-        <div>
-            <h1>Travelling Multiple Counteries</h1>
+        <>
+        <h1>Travelling Multiple Counteries</h1>
+        <h3>Visited Country:{visitedCounteries.length}</h3>
+        <h4>Visited Countries List: </h4> 
+        <ol>
             {
-                data.map(element => <CountryName key={element.name.official} name ={element.name.common} img={element.flags.png}></CountryName> )
+                visitedCounteries.map(element => <li>{element}</li>)
             }
-            <p></p>
-            
+            </ol>
+        <div className='gridCounteries'>            
+            {
+                data.map(element => <CountryName handleVisitedCountries={handleVisitedCountries}  key={element.name.official} name ={element.name.common} img={element.flags.png} element={element}></CountryName> )
+            }
+                       
         </div>
+        </>
+        
     );
 };
 
--- CounteriesProject/src/Counteries/CountryName.jsx
+++ CounteriesProject/src/Counteries/CountryName.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './CounteriesBorder.css'
+import './Button.css'
 
-const CountryName = ({name,img}) => {
+const CountryName = ({name,img,handleVisitedCountries,element}) => {
+
+    const [visited,setVisited] = useState(false);
+    
+
+    const visitedBtn =()=>{
+            setVisited(!visited);
+            handleVisitedCountries(element.name.common);
+            
+    }
     return (
-        <div>
+        <div className={`counterisBorder ${ visited && 'background'}`}>
             <h4>{name}</h4> <img src={img} alt="" />
+            <button onClick={visitedBtn} className='btn'>
+                {
+                    visited? 'Visited' : "Visit"
+                }
+            </button>
         </div>
     );
 };
